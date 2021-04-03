220a00ad13ce1c03f08122ddd6af5de1c04d4087
Muhammad Awais <awaisshaikh94@gmail.com>
2021-04-03 19:15:02 +0500

    added back navigation servive with back capability in header

---
+++ src/app/core/services/back-navigate/back-navigate.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackNavigateService {
+
+  public back = new Subject<boolean>();
+  visible: boolean = false;
+
+  constructor() { }
+
+  toggleBackState() {
+    this.visible = !this.visible;
+    this.back.next(this.visible);
+  }
+}
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
+import { Router } from '@angular/router';
+import{ BackNavigateService } from '../../core/services/back-navigate/back-navigate.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  backBtnState: boolean = false;
+
+  constructor(
+    private router: Router,
+    private backNavigateService: BackNavigateService,
+    private location: Location,
+  ) { }
 
   ngOnInit(): void {
+    this.backNavigateService.back.subscribe(res => {
+      this.backBtnState = res;
+    });
+  }
+
+  toggleBack() {
+    this.backNavigateService.toggleBackState();
+  }
+
+  back() {
+    this.location.back();
+    this.toggleBack();
   }
 
 }
